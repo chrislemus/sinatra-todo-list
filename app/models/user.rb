76f11a9ab64ed677d47b3406a1133db11f0a338a
chrislemus <icrislemus@gmail.com>
2021-02-04 21:56:44 -0500

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tasks
-
+  #test commment 
+  
   # def username=(username)
   #   self.slug = username.downcase.gsub(/[^a-z0-9\s]/, '').gsub(' ', '-')
   #   super(username)
