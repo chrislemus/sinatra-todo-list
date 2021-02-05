ff09b9d2e97f56874c5732027d2080b07115ca6a
chrislemus <icrislemus@gmail.com>
2021-02-04 22:02:09 -0500

    default branch changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tasks
-  #test commment 1
+  #test commment 
 
   # def username=(username)
   #   self.slug = username.downcase.gsub(/[^a-z0-9\s]/, '').gsub(' ', '-')
